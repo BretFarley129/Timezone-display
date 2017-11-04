efbbbf992a810cf6d72a1540a0c6d17de9cfc9cd
Bret Farley <bretfarley12@mittymonarchl.com>
2017-11-03 20:30:57 -0700

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  thing = Date.now();
+  STbool = 0;
+  PST(){
+    this.thing = Date.now();
+    this.thing -= 7*3600000
+    this.a = new Date(this.thing);
+    this.time = this.a.toUTCString();
+    this.STbool = 1;  
+  }
+  MST(){
+    this.thing = Date.now();
+    this.thing -= 6*3600000
+    this.a = new Date(this.thing);
+    this.time = this.a.toUTCString();  
+    this.STbool = 2;  
+  }
+  CST(){
+    this.thing = Date.now();
+    this.thing -= 5*3600000
+    this.a = new Date(this.thing);
+    this.time = this.a.toUTCString();  
+    this.STbool = 3;  
+  }
+  EST(){
+    this.thing = Date.now();
+    this.thing -= 4*3600000
+    this.a = new Date(this.thing);
+    this.time = this.a.toUTCString();  
+    this.STbool = 4;  
+  }
+  clear(){
+    this.thing = Date.now();
+    this.a = new Date(this.thing);
+    this.time = this.a.toUTCString();  
+    this.STbool = 0;  
+  }
+  a = new Date(this.thing);
+  time = this.a.toUTCString();  
+  
 }
